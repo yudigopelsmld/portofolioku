18390a5cee3b72c23453461b898c1859baf26e85
yudi <yudigopels@gmail.com>
2022-07-26 00:38:12 -0700

    Base Template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link, Switch, Redirect } from "react-router-dom";
+//import axios from 'axios';
+//import Navbar from './components/Navbar/Navbar';
+//import Header from './pages/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Portofolio from './pages/Portofolio';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
-function App() {
+class App extends Component {
+/*
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isiToken: '',
+      isiEmail: 'cvmiladiyyah@gmail.com',
+      isiPassword: 'arbiamel',
+    };
+  }
+/*
+  componentDidMount() {
+    //this.setState({ token:'dfdd' });
+    //console.log(token);
+    var data = {
+      email: this.state.isiEmail,
+      password: this.state.isiPassword
+  }
+    axios
+      .post('http://127.0.0.1:8000/api/login', data)
+      .then(response => {
+        console.log(response.data);
+        this.setState({ token: response.data.token });
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
+      
+  }
+  */
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+    <i className="bi bi-list mobile-nav-toggle d-xl-none"></i>
+        <header id="header">
+            <div className="d-flex flex-column">
+                <div className="profile">
+                <img src="assets/img/profile-img.jpg" alt="" className="img-fluid rounded-circle"/>
+                <h1 className="text-light"><Link to="/">Alex Smith</Link></h1>
+                    <div className="social-links mt-3 text-center">
+                        <Link to="#" className="twitter"><i className="bx bxl-twitter"></i></Link>
+                        <Link to="#" className="facebook"><i className="bx bxl-facebook"></i></Link>
+                        <Link to="#" className="instagram"><i className="bx bxl-instagram"></i></Link>
+                        <Link to="#" className="google-plus"><i className="bx bxl-skype"></i></Link>
+                        <Link to="#" className="linkedin"><i className="bx bxl-linkedin"></i></Link>
+                    </div>
+                </div>
+
+                <nav id="navbar" className="nav-menu navbar">
+                <ul>
+                <li><Link to="/home" className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Home</span></Link></li>
+                <li><Link to="/about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About</span></Link></li>
+                <li><Link to="/resume" className="nav-link scrollto"><i className="bx bx-file-blank"></i> <span>Resume</span></Link></li>
+                <li><Link to="/portofolio" className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Portfolio</span></Link></li>
+                <li><Link to="/services" className="nav-link scrollto"><i className="bx bx-server"></i> <span>Services</span></Link></li>
+                <li><Link to="/contact" className="nav-link scrollto"><i className="bx bx-envelope"></i> <span>Contact</span></Link></li>
+                </ul>
+                </nav>
+            </div>
+        </header>
+        
+
+        <main id="main">
+        <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/resume" element={<Resume/>}></Route>
+        <Route path="/portofolio" element={<Portofolio/>}></Route>
+        <Route path="/services" element={<Services/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        </Routes>
+        </main>
     </div>
+    </Router>
   );
+}
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//import { BrowserRouter as Router, Route, Routes, Link, Switch, Redirect } from "react-router-dom";
+//import Header from './Header';
+//import Navbar from './Navbar';
+//import axios from 'axios';
+//import { useState, useEffect } from "react";
+//import './Navbar.css'
+
+const Home = () => (
+    <div>
+        <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+            <div className="hero-container" data-aos="fade-in">
+                <h1>Alex Smith</h1>
+                <p>I'm Designer, Developer, Freelancer, Photographer</p>
+            </div>
+        </section>
+    </div>
+    )
+
+export default Home
